deb1361ec675c1e768fc369d618b0e118e6c019b
Amanda Westerlund <ext-amanda.westerlund@se.fujitsu.com>
2018-08-08 23:23:53 +0200

    First commit, before cleanup

---
+++ TheGame/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TetrisUI;
+using Type = System.Type;
+
+namespace TheGame
+{
+
+
+    public partial class Form1 : GameBoard
+    {
+        public List<Shape> Shapes { get; } = new List<Shape>();
+
+        public delegate void GameUpdatedEventHandler(object source, EventArgs args);
+        public event GameUpdatedEventHandler GameUpdated;
+
+        protected virtual void OnGameUpdated()
+        {
+            if (GameUpdated != null)
+            {
+                GameUpdated(this, EventArgs.Empty);
+            }
+        }
+
+        public Form1() : base(1000)
+        {
+            Shapes.Add(new Shape(ShapeType.I, ShapeRotation.Zero, ShapeColor.Cyan, 0, 0));
+            Shapes.Add(new Shape(ShapeType.I, ShapeRotation.Ninety, ShapeColor.Red, 0, 0));
+        }
+
+        protected override void UpdateGame()
+        {
+            foreach (var shape in Shapes)
+            {
+                shape.YPosition++;
+            }
+        }
+
+        protected void UpdateGame(IRender render)
+        {
+            MessageBox.Show("sda");
+            //OnGameUpdated();
+            //shape.YPosition++;
+            //Render(shapeI);
+            //shapeI.Draw(render);
+            //UpdatePosition();
+            //throw new NotImplementedException();
+        }
+
+        protected void UpdatePosition(IRender render, Shape shapeI)
+        {
+            MessageBox.Show("hej");
+            shapeI.YPosition++;
+            shapeI.Draw(render);
+            UpdateGame();
+        }
+
+        protected void MoveDownInSpeedOfGame(object source, EventArgs args)
+        {
+            //shapeI.YPosition++;
+        }
+        protected override void Render(IRender render)
+        {
+            foreach (var shape in Shapes)
+            {
+                shape.Draw(render);
+            }
+
+            //var i = 10;
+            //render.Draw(3, 3 + i, ShapeColor.Cyan);
+            //render.Draw(3, 4 + i, ShapeColor.Cyan);
+            //render.Draw(3, 5 + i, ShapeColor.Cyan);
+            //render.Draw(3, 6 + i, ShapeColor.Cyan);
+
+            //render.Draw(3, 0, ShapeColor.Cyan);
+            //render.Draw(4, 0, ShapeColor.Cyan);
+            //render.Draw(5, 0, ShapeColor.Cyan);
+            //render.Draw(6, 0, ShapeColor.Cyan);
+
+            //render.Draw(0, 1, ShapeColor.Orange);
+            //render.Draw(1, 1, ShapeColor.Orange);
+            //render.Draw(2, 1, ShapeColor.Orange);
+            //render.Draw(2, 0, ShapeColor.Orange);
+
+            //render.Draw(3, 8, ShapeColor.Blue);
+            //render.Draw(3, 9, ShapeColor.Blue);
+            //render.Draw(4, 9, ShapeColor.Blue);
+            //render.Draw(5, 9, ShapeColor.Blue);
+            
+            //render.Draw(9,19, ShapeColor.Red);
+
+            //var shapeI = new Shape(ShapeType.I, ShapeRotation.Zero, ShapeColor.Cyan, 0, 0);
+            //var shapeI90 = new Shape(ShapeType.I, ShapeRotation.Ninety, ShapeColor.Cyan, 0, 0);
+            //var shapeI180 = new Shape(ShapeType.I, ShapeRotation.OneEighty, ShapeColor.Cyan, 0, 0);
+            //var shapeI270 = new Shape(ShapeType.I, ShapeRotation.TwoSeventy, ShapeColor.Cyan, 0, 0);
+            //shapeI.XPosition++;
+            //shapeI.YPosition++;
+            //shapeI.Draw(render);
+            //shapeI90.Draw(render);
+            //shapeI180.Draw(render);
+            //UpdatePosition(render, shapeI);
+            //GameUpdated += MoveDownInSpeedOfGame;
+            
+            //shapeI270.Draw(render);
+            //throw new NotImplementedException();
+        }
+
+        protected override void Rotate()
+        {
+            foreach (var shape in Shapes)
+            {
+                shape.Rotate();
+            }
+        }
+
+        protected override void Drop()
+        {
+            throw new NotImplementedException("Drop");
+        }
+
+        protected override void MoveLeft()
+        {
+            
+            throw new NotImplementedException("Left");
+        }
+
+        protected override void MoveRight()
+        {
+            var shape = new Shape();
+            shape.XPosition++;
+            //throw new NotImplementedException("Right");
+        }
+    }
+}
---
+++ TheGame/Shape.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using TetrisUI;
+
+namespace TheGame
+{
+    public enum ShapeType
+    {
+        I,
+        J,
+        L,
+        O,
+        S,
+        T,
+        Z
+    }
+
+    public enum ShapeRotation
+    {
+        Zero,
+        Ninety,
+        OneEighty,
+        TwoSeventy
+    }
+
+    public class Shape
+    {
+        public ShapeType Type { get; }
+        public ShapeColor Color { get; }
+        public ShapeRotation Rotation { get; set; }
+        public int XPosition { get; set; }
+        public int YPosition { get; set; }
+
+        public Shape()
+        {
+        }
+
+        public Shape(ShapeType type, ShapeRotation rotation, ShapeColor color, int xPosition, int yPosition)
+        {
+            Type = type;
+            Rotation = rotation;
+            Color = color;
+            XPosition = xPosition;
+            YPosition = yPosition;
+        }
+
+        public void Rotate()
+        {
+            if (Rotation == ShapeRotation.TwoSeventy)
+            {
+                Rotation = ShapeRotation.Zero;
+            }
+            else
+            {
+                Rotation++;
+            }
+        }
+
+        public void Draw(IRender render)
+        {
+            switch (Type)
+            {
+                case ShapeType.I:
+                    DrawI(render);
+                    break;
+                case ShapeType.J:
+                    break;
+                case ShapeType.L:
+                    break;
+                case ShapeType.O:
+                    break;
+                case ShapeType.S:
+                    break;
+                case ShapeType.T:
+                    break;
+                case ShapeType.Z:
+                    break;
+            }
+        }
+
+        private void DrawI(IRender render)
+        {
+            switch (Rotation)
+            {
+                case ShapeRotation.Zero:
+                    DrawIZero(render);
+                    break;
+                case ShapeRotation.Ninety:
+                    DrawINinety(render);
+                    break;
+                case ShapeRotation.OneEighty:
+                    DrawIOneEighty(render);
+                    break;
+                case ShapeRotation.TwoSeventy:
+                    DrawITwoSeventy(render);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void ShapePosition(IRender render)
+        {
+
+        }
+        private void DrawIZero(IRender render)
+        {
+            render.Draw(XPosition + 3, YPosition + 1, ShapeColor.Cyan);
+            render.Draw(XPosition + 4, YPosition + 1, ShapeColor.Cyan);
+            render.Draw(XPosition + 5, YPosition + 1, ShapeColor.Cyan);
+            render.Draw(XPosition + 6, YPosition + 1, ShapeColor.Cyan);
+        }
+
+        private void DrawINinety(IRender render)
+        {
+            render.Draw(XPosition + 5, YPosition + 0, ShapeColor.Cyan);
+            render.Draw(XPosition + 5, YPosition + 1, ShapeColor.Cyan);
+            render.Draw(XPosition + 5, YPosition + 2, ShapeColor.Cyan);
+            render.Draw(XPosition + 5, YPosition + 3, ShapeColor.Cyan);
+        }
+
+        private void DrawIOneEighty(IRender render)
+        {
+            render.Draw(XPosition + 3, YPosition + 2, ShapeColor.Cyan);
+            render.Draw(XPosition + 4, YPosition + 2, ShapeColor.Cyan);
+            render.Draw(XPosition + 5, YPosition + 2, ShapeColor.Cyan);
+            render.Draw(XPosition + 6, YPosition + 2, ShapeColor.Cyan);
+        }
+
+        private void DrawITwoSeventy(IRender render)
+        {
+            render.Draw(XPosition + 4, YPosition + 0, ShapeColor.Cyan);
+            render.Draw(XPosition + 4, YPosition + 1, ShapeColor.Cyan);
+            render.Draw(XPosition + 4, YPosition + 2, ShapeColor.Cyan);
+            render.Draw(XPosition + 4, YPosition + 3, ShapeColor.Cyan);
+        }
+    }
+}
